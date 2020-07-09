ffe61bbfcecbe0adb287764f405394f02cbc2540
Dakota Brown <41762023+cdakotabrown@users.noreply.github.com>
2020-07-08 22:53:27 -0500

    Update bulb.py

--- bulb.py
+++ bulb.py
@@ -42,6 +42,7 @@ print("Bulb control running")
 
 bulb = init_bulb()
 # bulb.set_temperature(1)
+bulb.set_brightness(1)
 print(bulb.name)
 print(bulb.id)
 print(bulb.type)
@@ -51,8 +52,5 @@ print(bulb.temperature)
 while (True):
     c = get_dot_color()
     bulb.set_rgb(c[0], c[1], c[2])
-    bulb.set_brightness(1)
-    bulb.set_brightness(100)
-    bulb.set_brightness(1)
     time.sleep(60)
 
